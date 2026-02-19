350bc40ffc059d6106fa1d093d30c9fdda3d7883
Zachary Frame <framez@byu.edu>
2026-02-19 10:45:03 -0700

    homework work

--- hw/homework03/Grid.py
+++ hw/homework03/Grid.py
@@ -5,8 +5,10 @@ class Grid:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-
-
+        self.array = []
+        for y in range(height):
+            row = [None] * self.width
+            self.array.append(row)
 
     def in_bounds(self, x, y):
         """
@@ -21,7 +23,12 @@ class Grid:
         >>> grid.in_bounds(3, 4)
         False
         """
-        return True
+        if y >= len(self.array) or (y < 0):
+            return False
+        elif (x >= len(self.array[0])) or (x < 0):
+            return False
+        else:
+            return True
 
     def get(self, x, y):
         """
@@ -34,7 +41,10 @@ class Grid:
         >>> grid.get(1, 0)
         2
         """
-        return True
+        if self.in_bounds(x,y):
+            return self.array[y][x]
+        else:
+            raise IndexError
 
     def set(self, x, y, val):
         """
@@ -46,62 +56,67 @@ class Grid:
         >>> grid.array
         [[None, 'Dud'], [None, 'Milk']]
         """
-        return True
+        if self.in_bounds(x,y):
+            self.array[y][x] = val
+        else:
+            raise IndexError
     
-    @staticmethod
-    def check_list_malformed(lst):
-        """
-        Given a list that represents a 2D nested Grid, check that it has the
-        right shape. Raise a ValueError if it is malformed.
-        >>> Grid.check_list_malformed([[1, 2], [4, 5]])
-        >>> Grid.check_list_malformed(1)
-        Traceback (most recent call last):
-        ...
-        ValueError: Input must be a non-empty list of lists.
-        >>> Grid.check_list_malformed([[1, 2], [4, 5, 6]])
-        Traceback (most recent call last):
-        ...
-        ValueError: All items in list must be lists of the same length.
-        >>> Grid.check_list_malformed([[1, 2], 3])
-        Traceback (most recent call last):
-        ...
-        ValueError: Input must be a list of lists.
-        """
-        return True
+    # @staticmethod
+    # def check_list_malformed(lst):
+    #     """
+    #     Given a list that represents a 2D nested Grid, check that it has the
+    #     right shape. Raise a ValueError if it is malformed.
+    #     >>> Grid.check_list_malformed([[1, 2], [4, 5]])
+    #     >>> Grid.check_list_malformed(1)
+    #     Traceback (most recent call last):
+    #     ...
+    #     ValueError: Input must be a non-empty list of lists.
+    #     >>> Grid.check_list_malformed([[1, 2], [4, 5, 6]])
+    #     Traceback (most recent call last):
+    #     ...
+    #     ValueError: All items in list must be lists of the same length.
+    #     >>> Grid.check_list_malformed([[1, 2], 3])
+    #     Traceback (most recent call last):
+    #     ...
+    #     ValueError: Input must be a list of lists.
+    #     """
+    #     return True
     
-    @staticmethod
-    def build(lst):
-        """
-        Given a list that represents a 2D nested Grid construct a Grid object.
-        Grid.build([[1, 2, 3], [4, 5 6]])
-        >>> Grid.build([[1, 2, 3], [4, 5, 6]]).array
-        [[1, 2, 3], [4, 5, 6]]
-        """
-        return True
+    # @staticmethod
+    # def build(lst):
+    #     """
+    #     Given a list that represents a 2D nested Grid construct a Grid object.
+    #     Grid.build([[1, 2, 3], [4, 5 6]])
+    #     >>> Grid.build([[1, 2, 3], [4, 5, 6]]).array
+    #     [[1, 2, 3], [4, 5, 6]]
+    #     """
+    #     self.lst = lst
+    #     return True
     
-    def __eq__(self, other):
-        """
-        >>> grid1 = Grid.build([[1, 1, 1], [2, 3, 5]])
-        >>> grid2 = Grid.build([[1, 1, 1], [2, 3, 5]])
-        >>> grid_lst = [[1, 1, 1], [2, 3, 5]]
-        >>> grid1 == grid2
-        True
-        >>> grid1 == grid_lst
-        True
-        """
-        return True
+    # def __eq__(self, other):
+    #     """
+    #     >>> grid1 = Grid.build([[1, 1, 1], [2, 3, 5]])
+    #     >>> grid2 = Grid.build([[1, 1, 1], [2, 3, 5]])
+    #     >>> grid_lst = [[1, 1, 1], [2, 3, 5]]
+    #     >>> grid1 == grid2
+    #     True
+    #     >>> grid1 == grid_lst
+    #     True
+    #     """
+    #     return True
     
-    def __str__(self):
-        """
-        >>> print(Grid(6, 2))
-        Grid(6, 2, first = None)
-        """
-        return True
+    # def __str__(self):
+    #     """
+    #     >>> print(Grid(6, 2))
+    #     Grid(6, 2, first = None)
+    #     """
+    #     return True
     
-    def __repr__(self):
-        """
-        >>> repr(Grid.build([[5, 5], [3, 2]]))
-        'Grid.build([[5, 5], [3, 2]])'
-        """
-        return True
+    # def __repr__(self):
+    #     """
+    #     >>> repr(Grid.build([[5, 5], [3, 2]]))
+    #     'Grid.build([[5, 5], [3, 2]])'
+    #     """
+    #     return f"Grid({self.width}, {self.height}, array={self.array})"
     
+my_grid = Grid(2,2)
