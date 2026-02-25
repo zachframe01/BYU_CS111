35025b069679191fc834912819279e871679b1e7
Zachary Frame <framez@byu.edu>
2026-02-25 09:43:16 -0700

    in class work

---
+++ in_class/higher_order_functions.py
@@ -0,0 +1,60 @@
+# first-order function 
+def cube(k):
+    return k ** 3
+
+# higher-order function
+def summation(n, term):
+    """Sum the first N terms of a sequence. TERM is a function
+       that takes a single argument and returns a result.
+    >>> summation(5, cube)
+    225
+    """
+    total = 0
+    k = 1
+    while k <= n:
+        total = total + term(k)
+        k = k + 1
+    return total
+
+print(summation(5,cube))
+
+# using a lambda function instead of a def'd function
+def summation(n, term):
+    """Sum the first N terms of a sequence. TERM is a function
+       that takes a single argument and returns a result.
+    >>> summation(5, cube)
+    225
+    """
+    total = 0
+    k = 1
+    while k <= n:
+        total = total + term(k)
+        k = k + 1
+    return total
+
+print(summation(5,lambda k: k ** 3))
+
+# now print the result of summation replacing 0 with your own lambda expression
+print(summation(5,lambda k: 0))
+
+
+# locally defined function - adder
+def make_adder(n):
+    """Return a function that takes one argument k
+       and returns k + n.
+    >>> add_three = make_adder(3)
+    >>> add_three(4)
+    7
+    """
+    def adder(k):
+        return k + n
+    return adder
+
+add_three = make_adder(3)
+print(add_three(4))
+print(make_adder(3)(4))
+
+# now try writing a higher order funtion that takes a first string as a parameter
+# and returns a function that when called with a second string, returns a string
+# consisting of the second string with the first string concatenated to the front and back,
+# e.g., first_string = '**', second_string = 'Note', returned_string = '**Note**'
---
+++ in_class/lambda_expressions.py
@@ -0,0 +1,46 @@
+# a lambda expression is a simple function definition that evaluates to a function
+# syntax:
+# lambda <parameters>: <expression>
+
+# is there a difference between:
+def square(x):
+    return x * x
+print(square(4))
+# and:
+square = lambda x: x * x
+print(square(4))
+# ?
+
+# conditional expressions: if predicate is True, value is consequent, else value is alternative
+# syntax:
+# <consequent> if <predicate> else <alternative>
+
+# is there a difference between:
+def not_negative(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+print(not_negative(5),not_negative(-5))
+# and:
+not_negative = lambda x: x if x >= 0 else -x
+print(not_negative(5),not_negative(-5))
+# ?
+
+
+# lambda expression with a conditional expression
+result1 = lambda x: "x is greater than 10" if x > 10 else "x is less than or equal to 10"
+print(result1(12))  # Output: x is greater than 10
+print(result1(8))   # Output: x is less than or equal to 10
+
+# lambda expression with nested conditional expressions
+result2 = lambda x: "x is greater than 10" if x > 10 else ("x is less than 10" if x < 10 else "x is equal to 10")
+print(result2(12))  # Output: x is greater than 10
+print(result2(8))   # Output: x is less than 10
+print(result2(10))  # Output: x is equal to 10
+
+# Now write a lambda expression that is assigned to "result" that produces the output in the comments below
+result = lambda x : None
+print(result("red"))    # Output: Stop!
+print(result("green"))  # Output: Go!
+print(result("yellow")) # Output: Proceed with caution!
