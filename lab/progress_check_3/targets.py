deb41d87f977147d7b35a8243a858e62e0ea0691
Zachary Frame <framez@byu.edu>
2026-03-19 13:45:35 -0600

    working on progress check

--- lab/progress_check_3/targets.py
+++ lab/progress_check_3/targets.py
@@ -1,4 +1,4 @@
-CLASS_CODE = "CS111_W26_LINKED_AROUND_TARGET"
+CLASS_CODE = "PLEASE_UPDATE_CLASS_CODE"
 
 
 class Link:
@@ -26,72 +26,35 @@ class Link:
         return string + str(self.first) + '>'
     
 def count_targets(link, targets):
-    return count_targets_iterative(link, targets)
+    # return count_targets_iterative(link, targets)
     # return count_targets_recursive(link, targets)
+    ...
 
 def count_targets_iterative(link, targets):
-    """WILL COUNT THE TARGETS ITERATIVELY. """
-    empty_dict = dict()
-    string_link = str(link)
-    string_link = string_link.split('>')
-    string_link = string_link[0]
-    string_link = string_link.split('<')
-    string_link = string_link[1]
-    string_link = string_link.split(' ')
-    for el1 in string_link:
-        for el2 in targets:
-            if el1 == el2:
-                empty_dict()
-                
-
+    """YOUR CODE HERE"""
+    pass
 
 def count_targets_recursive(link, targets):
-    """WILL COUNT THE TARGETS RECRUSIVELY"""
-    if link.rest == ():
-        for element in targets:
-            if element == link.first:
-                my_list.append(element)
-                my_dict.update(element=0)
-        return
-    else:
-        for element in targets:
-            if element == link.first:
-                my_list.append(element)
-        count_targets_recursive(link.rest, targets)
-    return
+    """YOUR CODE HERE"""
+
+    pass
 
 def remove_targets(link, targets):
-    if link.rest != ():
-        for element in targets:
-            if element == link.first:
-                Link.self.first = Link.self.rest
-                remove_targets(link.rest, targets)
-    elif link.rest == ():
-        return
-    else:
-        return 
+    """YOUR CODE HERE"""
 
 
-link = Link('c', Link(2, Link(2, Link('a', Link('b', Link(4, Link('t', Link(2, Link('s', Link(4))))))))))
-# count_targets_iterative(link, 2)
-my_list = []
-my_dict = {}
-print(count_targets_recursive(link, [2,4]))
-print(my_list)
-print(my_dict)
-my_dict.fromkeys(my_list)
-print(my_dict)
-mi = [1,2,3]
-md = {}
-print(md)
-# >>> targets = [2, 4, 'b']
-# >>> count_targets(link, targets)
-# {2: 3, 'b': 1, 4: 2}
-# >>> link = remove_targets(link, targets)
-# >>> print(link)
-# <c a t s>
-# >>> count_targets(link, targets)
-# {}
+if __name__ == "__main__":
+    my_list = [2,4,5]
+    second_list = [2,2,2,2,4,3,2]
+    my_dict = {}
+    for keys in my_list:
+        my_dict[keys]=0
+    print(my_dict)
+    for key in second_list:
+        if key in my_dict:
+            my_dict[key]+=1
+        else:
+            pass
 
-
-#
+    print(my_dict)
+            
