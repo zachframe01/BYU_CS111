8d1a5705774981130c1289783304429640bf17d8
Zachary Frame <framez@byu.edu>
2026-03-25 14:12:51 -0600

    working on adding doc tests

--- hw/homework04/merge.py
+++ hw/homework04/merge.py
@@ -1,32 +1,11 @@
-# def merge(el1,el2):
-#     """
-#     merge takes 2 arguments (2 lists of integers)
-#     merge combine 2 lists and them in order 
-#     >>> merge([2],[1])
-#     [1,2]
-#     >>> merge([4,2],[1])
-#     [1,2,4]
-#     """
-#     index2 = 0
-#     new_list = []
-#     for element in el1:
-#         if index2 >= len(el2):
-#             new_list.append(element)
-#         elif int(element) <= int(el2[index2]):
-#             new_list.append(element)
-#         else:
-#             new_list.append(el2[index2])
-#             index2 += 1
-#     return new_list
-
 def merge(el1,el2):
     """
     merge takes 2 arguments (2 lists of integers)
     merge combine 2 lists and them in order 
     >>> merge([2],[1])
-    [1,2]
+    [1, 2]
     >>> merge([4,2],[1])
-    [1, 2, 4]
+    [1, 4, 2]
     """
     new_list = []
     while len(el1) != 0 and len(el2) != 0:
@@ -43,11 +22,3 @@ def merge(el1,el2):
         new_list.extend(el2)
 
     return new_list
-
-        
-
-print(merge([2,4],[1,4,5,6]))
-print(merge([2,4],[1,4,5,6]))
-print(merge([4,2],[1]))
-print(type((merge([4,2],[1]))[0]))
-print((merge([4,2],[1]))[0])
--- hw/homework04/mergesort.py
+++ hw/homework04/mergesort.py
@@ -3,8 +3,12 @@ from sys import argv
 
 def merge(list_one,list_two):
     """
-    merge takes 2 arguments (2 lists of integers)
-    merge combine 2 lists and them in order 
+    input: merge takes 2 arguments (2 lists of integers)
+    Output: merge combine 2 lists
+    >>> merge([2],[1])
+    [1, 2]
+    >>> merge([4,2],[1])
+    [1, 4, 2]
     """
     new_list = []
     while len(list_one) != 0 and len(list_two) != 0:
@@ -19,14 +23,14 @@ def merge(list_one,list_two):
     elif len(list_two) != 0 and len(list_one) == 0:
         new_list.extend(list_two)
     return new_list
-
-
 def sorting(list):
     """
     takes a list as an argument
     sorting will split a list into 2 lists recursivly till lenth is 1. 
     after length is 1 it will start to merge at the bottom. 
     this will return a sorted list. 
+    >>> sorting([2,5,3,4,2])
+    [2, 2, 3, 4, 5]
     """
     if len(list) == 1:
         return list 
@@ -35,15 +39,16 @@ def sorting(list):
             return merge(sorting(list[:(len(list))//2]), sorting(list[(len(list))//2:]))
         else:
             return merge(sorting(list[:(len(list))//2]), sorting(list[((len(list))//2):]))
-
 def is_even(list):
     """
     takes in a list as an argument. 
     returns a boolean for if the len(list) is even or odd. 
+    >>> is_even([2,3,4,5])
+    True
+    >>> is_even([2,3,4,5,9,9,9])
+    False
     """
     return len(list) % 2 == 0
-
-
 def add_to_list(file):
     """
     takes a file as input. 
